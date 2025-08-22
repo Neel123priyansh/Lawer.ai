52737ea34b69ef5ef20dfceeb5a0c95da1178ad9
Neel123priyansh <neelpriyansh@gmail.com>
2025-08-22 22:38:12 +0530

    Streamlit Done

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import requests
+
+API_URL = "https://f52c519fa4fc.ngrok-free.app/ask"
+
+st.title("LLM Based QABot")
+btn = st.button("Create KnowledgeBase")
+if btn:
+    pass
+
+question = st.text_input("Question")
+
+# if question:
+#     chain = get_qa_chain()
+#     response = chain(question)
+#     st.header("Answer: ")
+#     st.write(response["result"])
+
+
+if st.button("Ask"):
+    if question:
+        response = requests.post(API_URL, json={"query": question})
+        answer = response.json().get("answer", "Error")
+        st.header("Answer:")
+        st.write(answer)        
